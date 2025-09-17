76245ed4a0364a26a2646933d43e43c969c034b0
AD1065 <ad1065@AD1065ui-MacBookPro.local>
2025-09-17 22:50:11 +0900

    프론트와 백 연결

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule); //NestFactory를 사용하여 AppModule을 생성
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Next.js dev 서버
+    credentials: true,
+  });
+
+  await app.listen(3001);
+  console.log(`🚀 Backend running on http://localhost:3001`);
 }
 bootstrap();
